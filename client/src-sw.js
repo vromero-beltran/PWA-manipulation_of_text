26b126106a300926ec998dfeb115f7716340712d
Victor <frozen711.vr.vr@gmail.com>
2024-01-12 15:57:52 -0800

    change names for package.json to JATE

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,87 +26,10 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-
-// Cache names
-const CACHE_NAME = 'my-cache';
-const CACHE_STATIC_NAME = 'static-cache';
-
-// Cached files
-const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/images/logo.png'
-];
-
-// Install service worker
-self.addEventListener('install', (evt) => {
-  console.log('Service worker installed');
-
-  evt.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Caching files');
-        cache.addAll(FILES_TO_CACHE);
-      })
-  );
-});
-
-// Activate service worker
-self.addEventListener('activate', (evt) => {
-  console.log('Service worker activated');
-
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== CACHE_NAME && key !== CACHE_STATIC_NAME) {
-          console.log('Deleting old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-
-  self.clients.claim();
-});
-
-// Fetch assets
-self.addEventListener('fetch', (evt) => {
-  console.log('Fetch event for ', evt.request.url);
-
-  if (evt.request.url.includes('/styles.')) {
-    // Cache CSS files
-    evt.respondWith(
-      caches.open(CACHE_STATIC_NAME)
-        .then(cache => {
-          return cache.match(evt.request)
-            .then(response => {
-              return response || fetch(evt.request)
-                .then(response => {
-                  cache.put(evt.request, response.clone());
-                  return response;
-                });
-            });
-        })
-    );
-  } else {
-    // Cache other assets
-    evt.respondWith(
-      caches.match(evt.request)
-        .then(cacheRes => {
-          return cacheRes || fetch(evt.request)
-            .then(fetchRes => {
-              return caches.open(CACHE_NAME)
-                .then(cache => {
-                  cache.put(evt.request.url, fetchRes.clone());
-                  return fetchRes;
-                })
-            });
-        })
-    );
-  }
-});
-
+registerRoute(
+  ({ request }) => request.destination === 'style' || request.destination ==='script' || request.destination === 'image',
+  new StaleWhileRevalidate({
+    cacheName:'static-resources',
+  })
+);
 registerRoute();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,27 +3,62 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js"
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist")
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "./client/src/index.html",
+        chunks: ["main"]
+      }),
+      new WebpackPwaManifest({
+        name: "My Goals",
+        short_name: "Goals",
+        description: "A simple app to track your goals",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        start_url: "/",
+        publicPath: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: path.resolve('assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: './src-sw.js',
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
